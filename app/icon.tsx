3f53ecfc86404b178bfdb4cccab5cff04bf15abd
Roxana Ivanov <ivanovroxana1988@gmail.com>
2026-08-12 13:44:57 +0300

    Update favicon to new BGV identity

--- app/icon.tsx
+++ app/icon.tsx
@@ -11,13 +11,13 @@ export default function Icon(){
       display:'flex',
       alignItems:'center',
       justifyContent:'center',
-      background:'#1B1B1B',
-      color:'#FAF9F6',
-      fontSize:22,
-      fontWeight:700,
-      letterSpacing:-1,
+      background:'#FAF9F6',
+      position:'relative',
+      fontFamily:'Georgia, serif',
     }}>
-      BGV<span style={{color:'#8FC7E8'}}>.</span>
+      <span style={{position:'absolute',left:8,top:7,color:'#28343D',fontSize:40,lineHeight:1}}>B</span>
+      <span style={{position:'absolute',left:27,top:19,color:'#78A6D3',fontSize:31,lineHeight:1}}>G</span>
+      <span style={{position:'absolute',left:43,top:17,color:'#78A6D3',fontSize:35,lineHeight:1,fontStyle:'italic'}}>V</span>
     </div>,
     {...size},
   )
