01c181635958a067b08b5479b7280b2e27b42983
Roxana Ivanov <ivanovroxana1988@gmail.com>
2026-08-13 20:25:45 +0300

    Replace Romanian About route view

--- app/(ro)/despre/page.tsx
+++
@@ -1,7 +0,0 @@
-import View, { generateMetadata as createMetadata } from '@/app/_views/despre/page'
-export function generateMetadata() {
-  return createMetadata({ searchParams: { lang: 'ro' } })
-}
-export default function Page() {
-  return <View searchParams={{ lang: 'ro' }} />
-}
