6d3a9c3296d8e5d27336643a7559a20ccf365f39
Roxana Ivanov <ivanovroxana1988@gmail.com>
2026-08-13 20:25:54 +0300

    Use redesigned Romanian About view

---
+++ app/(ro)/despre/page.tsx
@@ -0,0 +1,7 @@
+import View, { generateMetadata as createMetadata } from '@/app/_views/despre/page-ro'
+export function generateMetadata() {
+  return createMetadata({ searchParams: { lang: 'ro' } })
+}
+export default function Page() {
+  return <View searchParams={{ lang: 'ro' }} />
+}
