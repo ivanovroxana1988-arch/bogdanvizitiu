58753d2f68564c8ee9d799dfa4a8a746deb05758
Roxana Ivanov <ivanovroxana1988@gmail.com>
2026-08-13 15:47:20 +0300

    Use new portrait on Romanian decision article

---
+++ app/(ro)/insights/o-decizie-buna-incepe-inainte-sa-alegi/page.tsx
@@ -0,0 +1,26 @@
+import View, { generateMetadata as createMetadata } from '@/app/_views/insights/[slug]/page'
+
+const slug = 'o-decizie-buna-incepe-inainte-sa-alegi'
+
+export function generateMetadata() {
+  return createMetadata({ params: { slug }, searchParams: { lang: 'ro' } })
+}
+
+export default function Page() {
+  return (
+    <>
+      <style>{`
+        article > figure.editorial-image:first-child {
+          background-image: url('/images/editorial/decision-bogdan-stairs.svg');
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+        }
+        article > figure.editorial-image:first-child > img {
+          opacity: 0 !important;
+        }
+      `}</style>
+      <View params={{ slug }} searchParams={{ lang: 'ro' }} />
+    </>
+  )
+}
