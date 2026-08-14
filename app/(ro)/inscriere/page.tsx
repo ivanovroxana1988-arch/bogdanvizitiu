76ae3fd04763db98fa3ed8f33f88c9ae6d5344d8
Roxana Ivanov <ivanovroxana1988@gmail.com>
2026-08-14 22:05:15 +0300

    Add Romanian registration route

---
+++ app/(ro)/inscriere/page.tsx
@@ -0,0 +1,11 @@
+import View, { generateMetadata as createMetadata } from '@/app/_views/inscriere/page'
+
+type PageProps = { searchParams?: { course?: string } }
+
+export function generateMetadata({ searchParams }: PageProps) {
+  return createMetadata({ searchParams: { lang: 'ro', course: searchParams?.course } })
+}
+
+export default function Page({ searchParams }: PageProps) {
+  return <View searchParams={{ lang: 'ro', course: searchParams?.course }} />
+}
